e107a09e4b6be511b46e8684e5b84f4a21e84c20
VladyslavTretiak <54894036+Proxima-C@users.noreply.github.com>
2021-08-29 13:13:10 +0300

    add interfaces for repositories (DAL)

---
+++ DAL/Interfaces/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using DAL.Entities;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : BaseEntity
+    {
+        IQueryable<TEntity> GetAll();
+
+        TEntity GetById(int id);
+
+        Task<TEntity> GetByIdAsync(int id);
+
+        void Add(TEntity entity);
+
+        Task AddAsync(TEntity entity);
+
+        void DeleteById(int id);
+
+        Task DeleteByIdAsync(int id);
+
+        void Update(TEntity entity);
+    }
+}
---
+++ DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        ITestRepository TestRepository { get; }
+
+        ITestQuestionRepository TestQuestionRepository { get; }
+
+        ITestAnswerRepository TestAnswerRepository { get; }
+
+        Task SaveAsync();
+    }
+}
