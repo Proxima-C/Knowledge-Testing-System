7b3bd4037f1041ae79f81dccb96b64d8af94b852
VladyslavTretiak <54894036+Proxima-C@users.noreply.github.com>
2021-08-29 13:27:57 +0300

    add AuthOptions (Authorization)

---
+++ Authorization/AuthOptions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Authorization
+{
+    public class AuthOptions
+    {
+        public const string ISSUER = "KnowledgeTestingSystem_WebAPI_JWT_Server";
+        public const string AUDIENCE = "KnowledgeTestingSystem_WebAPI_JWT_Client";
+        const string KEY = "mysupersecret_secretkey!123";
+        public const int LIFETIME = 1;
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
+        }
+    }
+}
