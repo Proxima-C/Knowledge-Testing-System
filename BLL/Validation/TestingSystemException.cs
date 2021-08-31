4434375201a29e5677b6adf79ff24571b39084e7
VladyslavTretiak <54894036+Proxima-C@users.noreply.github.com>
2021-08-31 16:06:35 +0300

    add testing system exception

---
+++ BLL/Validation/TestingSystemException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BLL.Validation
+{
+    public class TestingSystemException : Exception
+    {
+        public string Property { get; protected set; }
+
+        public TestingSystemException() { }
+
+        public TestingSystemException(string message) : base(message) { }
+
+        public TestingSystemException(string message, string prop) : base(message)
+        {
+            Property = prop;
+        }
+    }
+}
