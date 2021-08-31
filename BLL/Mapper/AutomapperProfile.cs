270317deeb8de54c7eff99ae571f4ca5d4842c13
VladyslavTretiak <54894036+Proxima-C@users.noreply.github.com>
2021-08-31 16:05:29 +0300

    add automapper profile

---
+++ BLL/Mapper/AutomapperProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using BLL.DTO;
+using DAL.Entities;
+using System.Linq;
+
+namespace BLL.Mapper
+{
+    public class AutomapperProfile : Profile
+    {
+        public AutomapperProfile()
+        {
+            CreateMap<Test, TestDTO>()
+                .ForMember(dest => dest.TestQuestionsIds, opt => opt.MapFrom(src => src.TestQuestions.Select(e => e.Id)))
+                .ReverseMap();
+
+            CreateMap<TestQuestion, TestQuestionDTO>()
+                .ForMember(dest => dest.TestQuestionAnswersIds, opt => opt.MapFrom(src => src.TestQuestionAnswers.Select(e => e.Id)))
+                .ReverseMap();
+
+            CreateMap<TestAnswer, TestAnswerDTO>()
+                .ReverseMap();
+
+            CreateMap<TestStatistics, TestStatisticsDTO>()
+                .ReverseMap();
+        }
+    }
+}
