fbe3c409be73fe41bb2f87677bc8f1b3823e30ea
VladyslavTretiak <54894036+Proxima-C@users.noreply.github.com>
2021-08-31 13:06:35 +0300

    add test statistics entity

---
+++ DAL/Entities/TestStatistics.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DAL.Entities
+{
+    public class TestStatistics : BaseEntity
+    {
+        public double UserScore { get; set; }
+        public TimeSpan PassingTime { get; set; }
+        public bool IsPassed { get; set; }
+        public int TestId { get; set; }
+        public string UserId { get; set; }
+
+        public virtual Test Test { get; set; }
+        public virtual User User { get; set; }
+
+    }
+}
