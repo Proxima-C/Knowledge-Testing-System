c990a0befdeb9eed9fb1c5b2b8e8c5376dfc1ec2
VladyslavTretiak <54894036+Proxima-C@users.noreply.github.com>
2021-09-04 16:00:46 +0300

    add exception filter

---
+++ WebApi/ExceptionFilters/TestingSystemExceptionFilter.cs
@@ -0,0 +1,25 @@
+﻿using BLL.Validation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApi.ExceptionFilters
+{
+    public class TestingSystemExceptionFilter : IActionFilter, IOrderedFilter
+    {
+        public int Order { get; } = int.MaxValue - 10;
+
+        public void OnActionExecuting(ActionExecutingContext context) { }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (context.Exception is TestingSystemException exception)
+            {
+                context.Result = new ObjectResult(exception.Message)
+                {
+                    StatusCode = exception.Status,
+                };
+                context.ExceptionHandled = true;
+            }
+        }
+    }
+}
