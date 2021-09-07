f50a8ccecc11609e1e7bdaef7fd8600e870504a4
VladyslavTretiak <54894036+Proxima-C@users.noreply.github.com>
2021-09-07 22:16:36 +0300

    update IdentityContext

---
+++ Authorization/Data/IdentityContext.cs
@@ -0,0 +1,20 @@
+﻿using Authorization.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authorization.Data
+{
+    public class IdentityContext : IdentityDbContext<ApplicationUser>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Seed();
+        }
+    }
+}
