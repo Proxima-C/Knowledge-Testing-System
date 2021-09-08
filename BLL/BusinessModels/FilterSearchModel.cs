a9e0c7cb2cacc22b3ac2e646c6755f292eb08628
VladyslavTretiak <54894036+Proxima-C@users.noreply.github.com>
2021-09-08 19:52:07 +0300

    add filter search model

---
+++ BLL/BusinessModels/FilterSearchModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BLL.BusinessModels
+{
+    public class FilterSearchModel
+    {
+        public TimeSpan? MinTestDuration { get; set; } = null;
+        public TimeSpan? MaxTestDuration { get; set; } = null;
+    }
+}
