603b297047c87d9f4196b9c6b0cdc81d312a818a
VladyslavTretiak <54894036+Proxima-C@users.noreply.github.com>
2021-09-14 18:51:04 +0300

    add test service and components in app module

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -4,17 +4,30 @@ import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestService } from './services/test/test.service';
+import { TestComponent } from './test/test.component';
+import { QuestionComponent } from './question/question.component';
+import { AnswerComponent } from './answer/answer.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    QuestionComponent,
+    AnswerComponent,
+    StatisticsComponent,
+    UserComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [TestService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
