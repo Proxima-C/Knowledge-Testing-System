c5d0523acd76d73cfe778d3c86217649c0fdde89
VladyslavTretiak <54894036+Proxima-C@users.noreply.github.com>
2021-09-14 17:34:46 +0300

    update identity db seed data

--- KnowledgeTestingSystem/Authorization/Models/IdentityModelBuilderExtensions.cs
+++ KnowledgeTestingSystem/Authorization/Models/IdentityModelBuilderExtensions.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using System;
 
 namespace Authorization.Models
 {
     public static class IdentityModelBuilderExtensions
     {
+        private static readonly string AdminId = "1aa9cfc9-f6bf-4a28-a2f4-4e46a0555aa0";
+        private static readonly string AdminRoleId = "46396fb8-c21e-4f83-ba5b-eda06c3cb9ac";
+
+        private static readonly string AppUserId = "02174cf0–9412–4cfe-afbf-59f706d72cf6";
+        private static readonly string AppUserRoleId = "341743f0-asd2–42de-afbf-59kmkkmk72cf6";
+
         public static void Seed(this ModelBuilder modelBuilder)
         {
             SeedApplicationUsers(modelBuilder);
@@ -20,8 +25,13 @@ namespace Authorization.Models
             modelBuilder.Entity<IdentityUserRole<string>>().HasData(
                 new IdentityUserRole<string>()
                 {
-                    RoleId = "fab4fac1-c546-41de-aebc-a14da6895711",
-                    UserId = "1aa9cfc9-f6bf-4a28-a2f4-4e46a0555aa0"
+                    RoleId = AdminRoleId,
+                    UserId = AdminId
+                },
+                new IdentityUserRole<string>()
+                {
+                    RoleId = AppUserRoleId,
+                    UserId = AppUserId
                 }
             );
         }
@@ -29,24 +39,38 @@ namespace Authorization.Models
         private static void SeedRoles(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<IdentityRole>().HasData(
-                new IdentityRole() { Id = "fab4fac1-c546-41de-aebc-a14da6895711", Name = "Admin", ConcurrencyStamp = "1", NormalizedName = "Admin" },
-                new IdentityRole() { Id = "c7b013f0-5201-4317-abd8-c211f91b7330", Name = "User", ConcurrencyStamp = "2", NormalizedName = "User" }
+                new IdentityRole() { Id = AdminRoleId, Name = "Admin", ConcurrencyStamp = AdminRoleId, NormalizedName = "Admin".ToUpper() },
+                new IdentityRole() { Id = AppUserRoleId, Name = "User", ConcurrencyStamp = AppUserRoleId, NormalizedName = "User".ToUpper() }
             );
         }
 
         private static void SeedApplicationUsers(ModelBuilder modelBuilder)
         {
+            ApplicationUser admin = new ApplicationUser
+            {
+                Id = AdminId,
+                UserName = "admin",
+                Email = "admin@gmail.com",
+                EmailConfirmed = true,
+                NormalizedUserName = "admin".ToUpper(),
+                NormalizedEmail = "admin@gmail.com".ToUpper(),
+            };
+
             ApplicationUser user = new ApplicationUser
             {
-                Id = "1aa9cfc9-f6bf-4a28-a2f4-4e46a0555aa0",
-                UserName = "admin@gmail.com",
-                NormalizedUserName = "admin@gmail.com",
+                Id = AppUserId,
+                UserName = "user",
+                Email = "user@gmail.com",
+                EmailConfirmed = true,
+                NormalizedUserName = "user".ToUpper(),
+                NormalizedEmail = "user@gmail.com".ToUpper(),
             };
-            PasswordHasher<ApplicationUser> passwordHasher = new PasswordHasher<ApplicationUser>();
-            user.SecurityStamp = Guid.NewGuid().ToString();
-            user.PasswordHash = passwordHasher.HashPassword(user, "12345");
 
-            modelBuilder.Entity<ApplicationUser>().HasData(user);
+            PasswordHasher<ApplicationUser> passwordHasher = new PasswordHasher<ApplicationUser>();
+            admin.PasswordHash = passwordHasher.HashPassword(admin, "12345");
+            user.PasswordHash = passwordHasher.HashPassword(user, "55555");
+
+            modelBuilder.Entity<ApplicationUser>().HasData(admin, user);
         }
     }
 }
