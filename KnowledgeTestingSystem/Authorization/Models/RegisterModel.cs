2249e79c6dcaa491db1a7e321e2817615c2542fa
VladyslavTretiak <54894036+Proxima-C@users.noreply.github.com>
2021-09-15 18:41:31 +0300

    add register model

---
+++ KnowledgeTestingSystem/Authorization/Models/RegisterModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Authorization.Models
+{
+    public class RegisterModel
+    {
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
