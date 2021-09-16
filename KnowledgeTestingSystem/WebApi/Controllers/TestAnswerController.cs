dbc60d89bdcd3e6e3e4b91cfe03235aa3c12541d
VladyslavTretiak <54894036+Proxima-C@users.noreply.github.com>
2021-09-16 18:13:57 +0300

    add authorize attribute in test answer controller

--- KnowledgeTestingSystem/WebApi/Controllers/TestAnswerController.cs
+++ KnowledgeTestingSystem/WebApi/Controllers/TestAnswerController.cs
@@ -1,5 +1,6 @@
 ﻿using BLL.DTO;
 using BLL.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -31,6 +32,7 @@ namespace WebApi.Controllers
             return new ObjectResult(answer);
         }
 
+        [Authorize(Roles = "Admin, Moderator")]
         [HttpPost]
         public async Task<ActionResult<TestAnswerDTO>> Add([FromBody] TestAnswerDTO answer)
         {
@@ -38,6 +40,7 @@ namespace WebApi.Controllers
             return Ok(answer);
         }
 
+        [Authorize(Roles = "Admin, Moderator")]
         [HttpPut]
         public async Task<ActionResult<TestAnswerDTO>> Update(TestAnswerDTO answer)
         {
@@ -45,6 +48,7 @@ namespace WebApi.Controllers
             return Ok(answer);
         }
 
+        [Authorize(Roles = "Admin, Moderator")]
         [HttpDelete("{id}")]
         public async Task<ActionResult> Delete(int id)
         {
