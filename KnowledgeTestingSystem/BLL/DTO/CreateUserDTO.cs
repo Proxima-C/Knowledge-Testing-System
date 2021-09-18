58072071fe0b55cb736f8f33bf1d959dba880111
VladyslavTretiak <54894036+Proxima-C@users.noreply.github.com>
2021-09-19 00:32:46 +0300

    add user dto

---
+++ KnowledgeTestingSystem/BLL/DTO/CreateUserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace BLL.DTO
+{
+    public class CreateUserDTO
+    {
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ KnowledgeTestingSystem/BLL/DTO/UserDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace BLL.DTO
+{
+    public class UserDTO
+    {
+        public string UserId { get; set; }
+        public int ProfileId { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public ICollection<int> UserTestStatisticsIds { get; set; }
+    }
+}
