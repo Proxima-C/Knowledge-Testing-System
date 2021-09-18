0d9d5f5c77a8587bea2e672cab84e537b43bb799
VladyslavTretiak <54894036+Proxima-C@users.noreply.github.com>
2021-09-19 00:41:30 +0300

    update account controller

--- KnowledgeTestingSystem/WebApi/Controllers/AccountController.cs
+++ KnowledgeTestingSystem/WebApi/Controllers/AccountController.cs
@@ -1,15 +1,6 @@
-﻿using Authorization;
-using Authorization.Models;
-using BLL.DTO;
+﻿using Authorization.Models;
 using BLL.Interfaces;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace WebApi.Controllers
@@ -18,96 +9,25 @@ namespace WebApi.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
-        private readonly UserManager<AppUser> _userManager;
-        private readonly IUserProfileService _userProfileService;
+        private readonly IAccountService _accountService;
 
-        public AccountController(UserManager<AppUser> userManager, IUserProfileService userProfileService)
+        public AccountController(IAccountService accountService)
         {
-            _userManager = userManager;
-            _userProfileService = userProfileService;
+            _accountService = accountService;
         }
 
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody] LoginModel loginModel)
         {
-            var identity = await GetIdentityAsync(loginModel.Username, loginModel.Password);
-            if (identity == null) return BadRequest("Invalid username or password.");
-
-            var now = DateTime.UtcNow;
-
-            var jwt = new JwtSecurityToken(
-                    issuer: AuthOptions.Issuer,
-                    audience: AuthOptions.Audience,
-                    notBefore: now,
-                    expires: now.Add(TimeSpan.FromMinutes(AuthOptions.LifeTimeInMinutes)),
-                    claims: identity.Claims,
-                    signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(),
-                    SecurityAlgorithms.HmacSha256));
-            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
-
-            var response = new
-            {
-                access_token = encodedJwt,
-                username = identity.Name
-            };
-
+            var response = await _accountService.LoginAsync(loginModel);
             return Ok(response);
         }
 
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterModel model)
         {
-            var userExists = await _userManager.FindByNameAsync(model.Username);
-            if (userExists != null)
-                return BadRequest("Username already exists.");
-
-            AppUser user = new AppUser()
-            {
-                UserName = model.Username,
-                Email = model.Email
-            };
-            var result = await _userManager.CreateAsync(user, model.Password);
-
-            if (!result.Succeeded)
-                return BadRequest("User creation failed. Check user data and try again.");
-
-            UserProfileDTO userProfile = new UserProfileDTO()
-            {
-                UserName = model.Username,
-                Name = model.Name
-            };
-
-            await _userProfileService.AddAsync(userProfile);
-
+            await _accountService.RegisterAsync(model);
             return Ok("User created successfully.");
-        }
-
-        private async Task<ClaimsIdentity> GetIdentityAsync(string username, string password)
-        {
-            var user = await _userManager.FindByNameAsync(username);
-
-            if (!await _userManager.CheckPasswordAsync(user, password))
-            {
-                return null;
-            }
-
-            var claims = new List<Claim>
-                {
-                    new Claim(ClaimsIdentity.DefaultNameClaimType, user.UserName),
-                };
-
-            var roles = await _userManager.GetRolesAsync(user);
-
-            foreach (var role in roles)
-            {
-                claims.Add(new Claim(ClaimsIdentity.DefaultRoleClaimType, role));
-            }
-
-            var claimsIdentity = new ClaimsIdentity(claims, "Token",
-                ClaimsIdentity.DefaultNameClaimType,
-                ClaimsIdentity.DefaultRoleClaimType);
-
-            return claimsIdentity;
         }
     }
 }
